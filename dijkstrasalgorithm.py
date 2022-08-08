2fb94aa5fb1a4221df3ed4823f491654d4c42993
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2022-08-08 13:28:39 +0530

    made it smaller

--- dijkstrasalgorithm.py
+++ dijkstrasalgorithm.py
@@ -1,22 +1,7 @@
-graph = {}
-graph["start"] = {}
-graph["start"]["a"] = 6
-graph["start"]["b"] = 2
-graph['a'] = {}
-graph['a']['fin'] = 1
-graph['b'] = {}
-graph['b']['a'] = 3
-graph['b']['fin'] = 5
-graph['fin'] = {}
+graph = {}; graph["start"] = {}; graph["start"]["a"] = 6;graph["start"]["b"] = 2;graph['a'] = {};graph['a']['fin'] = 1;graph['b'] = {};graph['b']['a'] = 3;graph['b']['fin'] = 5;graph['fin'] = {}
 infinity = float('inf')
-costs = {}
-costs['a'] = 6
-costs['b'] = 2
-costs['fin'] = infinity
-parents = {}
-parents['a'] = 'start'
-parents['b'] = 'start'
-parents['fin'] = None
+costs = {};costs['a'] = 6;costs['b'] = 2;costs['fin'] = infinity
+parents = {};parents['a'] = 'start';parents['b'] = 'start';parents['fin'] = None
 processed = []
 
 def get_key(val):
