34abaa9beff8b6d9ea07e309209da33abb1db390
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2022-08-08 13:16:09 +0530

    From the quicksort chapter of the book
    This creates a quicksort algorithm that sorts a list of numbers

---
+++ quicksort.py
@@ -0,0 +1,10 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([2, 5, 4, 3, 6, 9, 8, 7, 1, 10]))
